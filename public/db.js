a401288e5164a43b4b1560abc91b66c8fe71f8cc
Maurya Patel <patelmaurya0512@gmail.com>
2020-09-14 23:17:02 -0400

    Added a function to check whether the app's online and connecting to the db

--- public/db.js
+++ public/db.js
@@ -1,3 +1,5 @@
+const { get } = require("mongoose");
+
 let db;
 // we create a new db request for a database
 const request = indexedDB.open("budget",1);
@@ -7,3 +9,26 @@ request.onupgradeneeded = function(event) {
     const db = event.target.result;
     db.createObjectStore("pending", {autoIncrement: true});
 };
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    // check if the app is online and having access to the database
+    if(navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    // access your "pending" object store
+    const store = transaction.objectStore("pending");
+    // get all records from store and set to a variable
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0) {
+            
+        }
+    }
+}
